9341c2c041449bb3f8bc00535dcc2fe178d256dc
edmond <644568491@qq.com>
2019-09-10 15:53:45 +0800

    refine eslintrc

--- node-koa-app.js
+++ node-koa-app.js
@@ -3,6 +3,22 @@
 const Koa = require('koa');
 const app = new Koa();
 const port = 3000;
+const X_RESPONSE_TIME = 'x-response-time';
+
+// logger
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.get(X_RESPONSE_TIME);
+    console.log(`${ctx.method} ${ctx.url} - ${rt}ms`);
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set(X_RESPONSE_TIME, ms);
+});
 
 app.use(async (ctx) => {
     ctx.body = 'Hello World';
